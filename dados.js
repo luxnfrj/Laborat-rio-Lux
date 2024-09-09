0868193bd77e00a70039afbd01829ee61579ff52
luxnfrj <168010169+luxnfrj@users.noreply.github.com>
2024-09-09 12:02:54 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+{
+    Título: "Espectrofotômetro UV-Vis",
+    Descrição: "O espectrofotômetro UV-Vis é um instrumento utilizado para medir a intensidade da luz absorvida por uma amostra em função do comprimento de onda. É amplamente utilizado em química analítica para quantificar substâncias e analisar reações químicas.",
+    Link: "https://www.thermofisher.com/br/pt/home/life-science/spectroscopy/uv-visible-spectroscopy.html",
+    Tags: "espectrofotometro equipamento intesidade da luz"
+},
+{
+    Título: "Centrífuga",
+    Descrição: "A centrífuga é um equipamento que utiliza a força centrífuga para separar componentes de uma amostra, como células, organelas e partículas. É essencial em diversas áreas, como biologia molecular e bioquímica.",
+    Link: "https://www.eppendorf.com/br-pt/centrifuges/",
+    Tags: "separação separacao forca centrifuga força centrífuga"
+},
+{
+    Título: "Micropipeta",
+    Descrição: "A micropipeta é um instrumento de precisão utilizado para transferir volumes muito pequenos de líquidos. É amplamente utilizada em laboratórios de biologia molecular e química.",
+    Link: "https://www.gilson.com/br/pt/produtos/micropipetas",
+    Tags: "pipeta equipamento transferir volumes volume"
+}
+];
